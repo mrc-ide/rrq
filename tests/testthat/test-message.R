ffa16d07a112cfa816d2794f121861b04fee815b
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-02-09 08:27:00 +0000

    Style update and check through messaging

--- tests/testthat/test-message.R
+++ tests/testthat/test-message.R
@@ -1,13 +1,13 @@
 context("messaging")
 
 test_that("timeout", {
-  Sys.setenv(R_TESTS="")
+  Sys.setenv(R_TESTS = "")
   root <- tempfile()
-  context <- context::context_save(root, sources="myfuns.R")
+  context <- context::context_save(root, sources = "myfuns.R")
   obj <- rrq_controller(context, redux::hiredis())
   on.exit(obj$destroy())
 
-  wid <- workers_spawn(context, obj$con, logdir="logs", worker_time_poll=1)
+  wid <- workers_spawn(context, obj$con, logdir = "logs", worker_time_poll = 1)
 
   ## First, let's test the basic messaging approach:
   ##
@@ -27,9 +27,10 @@ test_that("timeout", {
 
   expect_equal(obj$response_ids(wid), id)
 
-  expect_equal(obj$get_responses(id, wait=10), setNames(list("PONG"), wid))
+  expect_equal(obj$get_responses(id, wait = 10), setNames(list("PONG"), wid))
 
-  expect_equal(obj$get_responses(id, delete=TRUE), setNames(list("PONG"), wid))
+  expect_equal(obj$get_responses(id, delete = TRUE),
+               setNames(list("PONG"), wid))
   expect_equal(obj$response_ids(wid), character(0))
   expect_equal(obj$has_response(id, wid), FALSE)
   expect_error(obj$get_response(id, wid), "Response missing")
@@ -38,35 +39,35 @@ test_that("timeout", {
 
   ## Next, echo:
   id <- obj$send_message("ECHO", "hello world")
-  expect_equal(obj$get_responses(id, wait=1), setNames(list("OK"), wid))
+  expect_equal(obj$get_responses(id, wait = 1), setNames(list("OK"), wid))
 
   ## Eval
   id <- obj$send_message("EVAL", "1 + 1")
-  expect_equal(obj$get_response(id, wid, wait=1), 2)
+  expect_equal(obj$get_response(id, wid, wait = 1), 2)
 
   id <- obj$send_message("EVAL", quote(1 + 1))
-  expect_equal(obj$get_response(id, wid, wait=1), 2)
+  expect_equal(obj$get_response(id, wid, wait = 1), 2)
 
   id <- obj$send_message("INFO")
-  res <- obj$get_response(id, wid, wait=1)
+  res <- obj$get_response(id, wid, wait = 1)
   expect_is(res, "worker_info")
   expect_equal(res$worker, wid)
   expect_equal(res$hostname, hostname())
 
   id <- obj$send_message("TIMEOUT_GET")
-  expect_equal(obj$get_response(id, wid, wait=1),
-               c(timeout=Inf, remaining=Inf))
+  expect_equal(obj$get_response(id, wid, wait = 1),
+               c(timeout = Inf, remaining = Inf))
 
   id <- obj$send_message("TIMEOUT_SET", 1000)
-  expect_equal(obj$get_response(id, wid, wait=1), "OK")
+  expect_equal(obj$get_response(id, wid, wait = 1), "OK")
 
   id <- obj$send_message("TIMEOUT_GET")
-  res <- obj$get_response(id, wid, wait=1)
+  res <- obj$get_response(id, wid, wait = 1)
   expect_equal(res[["timeout"]], 1000)
   expect_lte(res[["remaining"]], 1000)
 
   id <- obj$send_message("TIMEOUT_SET", 0)
-  expect_equal(obj$get_response(id, wid, wait=1), "OK")
+  expect_equal(obj$get_response(id, wid, wait = 1), "OK")
 
   Sys.sleep(1)
 
@@ -75,30 +76,30 @@ test_that("timeout", {
 })
 
 test_that("pause", {
-  Sys.setenv(R_TESTS="")
+  Sys.setenv(R_TESTS = "")
   root <- tempfile()
-  context <- context::context_save(root, sources="myfuns.R")
+  context <- context::context_save(root, sources = "myfuns.R")
   obj <- rrq_controller(context, redux::hiredis())
   on.exit(obj$destroy())
 
-  wid <- workers_spawn(context, obj$con, logdir="logs", worker_time_poll=1)
+  wid <- workers_spawn(context, obj$con, logdir = "logs", worker_time_poll = 1)
 
   expect_that(obj$workers_status(),
               equals(setNames(WORKER_IDLE, wid)))
 
   id <- obj$send_message("PAUSE")
-  expect_that(obj$get_response(id, wid, wait=1), equals("OK"))
+  expect_that(obj$get_response(id, wid, wait = 1), equals("OK"))
 
   expect_that(obj$workers_status(),
               equals(setNames(WORKER_PAUSED, wid)))
 
   ## Can still ping the worker quite happily:
   id <- obj$send_message("PING")
-  expect_that(obj$get_response(id, wid, wait=1), equals("PONG"))
+  expect_that(obj$get_response(id, wid, wait = 1), equals("PONG"))
 
   ## Pausing again is a noop:
   id <- obj$send_message("PAUSE")
-  expect_that(obj$get_response(id, wid, wait=1), equals("NOOP"))
+  expect_that(obj$get_response(id, wid, wait = 1), equals("NOOP"))
 
   t <- obj$enqueue(sin(1))
   ## should have been queued by now if the worker was interested:
@@ -107,7 +108,7 @@ test_that("pause", {
   expect_equal(obj$task_status(t), TASK_PENDING)
 
   id <- obj$send_message("RESUME")
-  expect_that(obj$get_response(id, wid, wait=1), equals("OK"))
+  expect_that(obj$get_response(id, wid, wait = 1), equals("OK"))
 
   res <- obj$task_wait(t, 1)
   expect_equal(res, sin(1))
@@ -124,48 +125,48 @@ test_that("pause", {
                                     "TASK_START", "TASK_COMPLETE")))
   expect_that(log$message, equals(c("",
                                     rep(c("PAUSE", "PING", "PAUSE", "RESUME"),
-                                        each=2),
+                                        each = 2),
                                     t, t)))
 
   ## Will stop when paused:
   id <- obj$send_message("PAUSE")
-  expect_that(obj$get_response(id, wid, wait=1), equals("OK"))
+  expect_that(obj$get_response(id, wid, wait = 1), equals("OK"))
   id <- obj$send_message("STOP")
-  expect_that(obj$get_response(id, wid, wait=1), equals("BYE"))
+  expect_that(obj$get_response(id, wid, wait = 1), equals("BYE"))
 
   expect_that(obj$workers_list_exited(), equals(wid))
 })
 
 test_that("unknown command", {
-  Sys.setenv(R_TESTS="")
+  Sys.setenv(R_TESTS = "")
   root <- tempfile()
-  context <- context::context_save(root, sources="myfuns.R")
+  context <- context::context_save(root, sources = "myfuns.R")
   obj <- rrq_controller(context, redux::hiredis())
   on.exit(obj$destroy())
 
-  wid <- workers_spawn(context, obj$con, logdir="logs", worker_time_poll=1)
+  wid <- workers_spawn(context, obj$con, logdir = "logs", worker_time_poll = 1)
 
   expect_that(obj$workers_status(),
               equals(setNames(WORKER_IDLE, wid)))
 
   id <- obj$send_message("XXXX")
-  res <- obj$get_response(id, wid, wait=1)
+  res <- obj$get_response(id, wid, wait = 1)
   expect_that(res, is_a("condition"))
   expect_that(res$message, matches("Recieved unknown message"))
   expect_that(res$command, equals("XXXX"))
   expect_that(res$args, equals(NULL))
 
   id <- obj$send_message("YYYY", "ZZZZ")
-  res <- obj$get_response(id, wid, wait=1)
+  res <- obj$get_response(id, wid, wait = 1)
   expect_that(res, is_a("condition"))
   expect_that(res$message, matches("Recieved unknown message"))
   expect_that(res$command, equals("YYYY"))
   expect_that(res$args, equals("ZZZZ"))
 
   ## Complex arguments are supported:
-  d <- data.frame(a=1, b=2)
+  d <- data.frame(a = 1, b = 2)
   id <- obj$send_message("YYYY", d)
-  res <- obj$get_response(id, wid, wait=1)
+  res <- obj$get_response(id, wid, wait = 1)
   expect_that(res, is_a("condition"))
   expect_that(res$message, matches("Recieved unknown message"))
   expect_that(res$command, equals("YYYY"))
