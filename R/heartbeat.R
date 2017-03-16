e40e3e5e26ae3511e0941fb22f6096ecc3d2b033
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-03-16 18:41:37 +0000

    Start adding support for detecting expired workers

--- R/heartbeat.R
+++ R/heartbeat.R
@@ -10,3 +10,57 @@ heartbeat <- function(con, key, period) {
     ret
   }
 }
+
+heartbeat_time_remaining <- function(obj) {
+  worker_id <- obj$worker_list()
+  if (length(worker_id) > 0L) {
+    key <- rrq_key_worker_heartbeat(obj$keys$queue_name, worker_id)
+
+    ## Do _all_ this in one block for consistency I think
+    status <- obj$worker_status()
+    time <- vnapply(key, obj$con$PTTL)
+    task_id <- obj$worker_task_id(worker_id)
+
+    ## Turn time into seconds:
+    alive <- time > 0
+    time[alive] <- time[alive] / 1000
+  } else {
+    time <- numeric(0)
+    status <- task_id <- character(0)
+  }
+
+  ret <- data.frame(worker_id = worker_id,
+                    time = time,
+                    status = status,
+                    task_id = task_id,
+                    stringsAsFactors = FALSE)
+  rownames(ret) <- NULL
+  ret
+}
+
+identify_orphan_tasks <- function(obj) {
+  dat <- heartbeat_time_remaining(obj)
+  cleanup_orphans(obj, dat)
+}
+
+cleanup_orphans <- function(obj, dat) {
+  i <- dat$time < 0
+  if (any(i)) {
+    task_id <- dat$task_id[i]
+    task_id <- task_id[!is.na(task_id)]
+    worker_id <- dat$worker_id[i]
+    message(sprintf(
+      "Lost %s %s:\n%s",
+      length(worker_id), ngettext(length(worker_id), "worker", "workers"),
+      paste0("  - ", worker_id, collapse = "\n")))
+    message(sprintf(
+      "Orphaning %s %s:\n%s",
+      length(task_id), ngettext(length(task_id), "task", "tasks"),
+      paste0("  - ", task_id, collapse = "\n")))
+    obj$con$HMSET(obj$keys$task_status, dat$task_id[i], TASK_ORPHAN)
+    obj$con$HMSET(obj$keys$worker_status, dat$worker_id[i], WORKER_LOST)
+    obj$con$SREM(obj$keys$worker_name, dat$worker_id[i])
+  }
+  ret <- dat[i, c("worker_id", "task_id"), drop = FALSE]
+  ret
+}
--- tests/testthat/test-zzz-fault-tolerance.R
+++ tests/testthat/test-zzz-fault-tolerance.R
@@ -131,3 +131,60 @@ test_that("interrupt stuck worker (via heartbeat)", {
   expect_equal(tmp$command,
                c("REQUEUE", "MESSAGE", "RESPONSE"))
 })
+
+test_that("detect killed worker (via heartbeat)", {
+  Sys.setenv(R_TESTS = "")
+  root <- tempfile()
+  context <- context::context_save(root, sources = "myfuns.R")
+  context <- context::context_load(context, new.env(parent = .GlobalEnv))
+
+  obj <- rrq_controller(context, redux::hiredis())
+  on.exit(obj$destroy())
+
+  ## We need to set time_poll to be fairly fast because BLPOP is not
+  ## interruptable; the interrupt will only be handled _after_ R gets
+  ## control back.
+  res <- obj$worker_config_save("localhost", time_poll = 1,
+                                heartbeat_period = 1,
+                                copy_redis = TRUE)
+
+  wid <- worker_spawn(obj, timeout = 5, progress = PROGRESS)
+  pid <- obj$worker_info()[[wid]]$pid
+
+  key <- rrq_key_worker_heartbeat(context$id, wid)
+  expect_equal(obj$con$EXISTS(key), 1)
+  expire <- res$heartbeat_period * 3
+  expect_equal(obj$con$GET(key), as.character(expire))
+  expect_lte(obj$con$TTL(key), expire)
+
+  t <- obj$enqueue(slowdouble(10000))
+  wait_status(t, obj, status = TASK_PENDING)
+  expect_equal(obj$task_status(t), setNames(TASK_RUNNING, t))
+  expect_equal(obj$worker_status(wid), setNames(WORKER_BUSY, wid))
+
+  tools::pskill(pid, tools::SIGTERM)
+  Sys.sleep(0.1)
+  expect_equal(obj$task_status(t), setNames(TASK_RUNNING, t))
+  expect_equal(obj$worker_status(wid), setNames(WORKER_BUSY, wid))
+
+  ## This is a bit annoying as it takes a while to run through;
+  Sys.sleep(expire)
+
+  ## Our key has gone!  Marvellous!
+  expect_equal(obj$con$EXISTS(key), 0)
+
+  expect_equal(obj$worker_list(), wid)
+  dat1 <- heartbeat_time_remaining(obj)
+  dat2 <- identify_orphan_tasks(obj)
+  expect_equal(obj$worker_list(), character(0))
+  dat3 <- heartbeat_time_remaining(obj)
+
+  cmp <- data.frame(worker_id = wid, time = -2, status = WORKER_BUSY,
+                    task_id = t, stringsAsFactors = FALSE)
+  expect_equal(dat1, cmp)
+  expect_equal(dat2, cmp[c("worker_id", "task_id")])
+  expect_equal(dat3, cmp[integer(0), ])
+
+  expect_message(cleanup_orphans(obj, dat1), "Lost 1 worker:")
+  expect_message(cleanup_orphans(obj, dat1), "Orphaning 1 task:")
+})
