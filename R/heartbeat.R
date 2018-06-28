b97c8c65802bf5fc814cb71ec4772be8d2713f63
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2018-06-28 12:17:53 +0100

    Update heartbeat call

--- R/heartbeat.R
+++ R/heartbeat.R
@@ -3,9 +3,7 @@ heartbeat <- function(con, key, period) {
     context::context_log("heartbeat", key)
     loadNamespace("heartbeatr")
     config <- con$config()
-    ret <- heartbeatr::heartbeat(key, period,
-                                 host = config$host, port = config$port,
-                                 password = config$password, db = config$db)
+    ret <- heartbeatr::heartbeat(key, period, config = config)
     context::context_log("heartbeat", "OK")
     ret
   }
