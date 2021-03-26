cc7776801a8f08fc60f817040ab5332e2c09eec8
Robert Ashton <r.ashton@imperial.ac.uk>
2021-03-26 15:04:59 +0000

    Convert task queue list to character vector

--- R/dependencies.R
+++ R/dependencies.R
@@ -39,7 +39,7 @@ queue_dependencies <- function(con, keys, deferred_set, task_id,
   ## Tasks with 0 remaining dependencies can be queued
   tasks_to_queue <- names(res[res == 0 & names(res) != ""])
   if (length(tasks_to_queue > 0)) {
-    task_queues <- con$HMGET(keys$task_queue, tasks_to_queue)
+    task_queues <- list_to_character(con$HMGET(keys$task_queue, tasks_to_queue))
     queue_keys <- rrq_key_queue(keys$queue_id, task_queues)
     queue_task <- function(id, queue_key) {
       list(
