599f1825d01f6956faeac018b86d041f617d6eac
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2021-04-14 19:27:46 +0100

    Start generalising the repl evaluator

--- R/repl.R
+++ R/repl.R
@@ -1,61 +1,115 @@
+## Once working, all usage of cat(...) can safely become
+## message(..., appendLF = FALSE) because the handling will dump that all
+## into one stream.
 repl_eval <- function(expr, envir, path) {
   withr::with_message_sink(path, {
     withr::with_output_sink(path, {
-      expression_eval_safely2(expr, envir)
+      res <- repl_eval_expression(expr, envir)
+      if (res$success) {
+        if (res$value$visible) {
+          print(res$value$value)
+        }
+      } else {
+        cat(as.character(res$error))
+      }
+      show_warnings(res$warnings, res$success)
+      res
     })
   })
 }
 
 
-with_autoprint <- function(expr) {
-  ans <- withVisible(force(expr))
-  if (ans$visible) {
-    print(ans$value)
-  }
-  ans$value
-}
-
-
-expression_eval_safely2 <- function(expr, envir) {
-  ## NOTE: basically the a generalised form of expression_eval_safely;
-  ## we could build that function easily from this one.
+## The next step here is getting the error/warning reporting working correctly.
+repl_eval_expression <- function(expr, envir) {
   warnings <- collector(list())
-  trace <- collector()
+  trace <- collector(list())
+  stack_skip <- length(sys.calls()) + 7L
 
-
-  handler <- function(e) {
-    error_target <- list(quote(expression_eval_safely2), quote(eval))
+  handle_error <- function(e) {
+    error_target <- list(quote(repl_eval_expression), quote(eval))
     list(success = FALSE,
          error = e,
          ## This does a good job of lopping off the leading mess, but
          ## we have 1-2 calls at the tail of the stack that are not
          ## here normally due to calling handlers. These vary based on
          ## (at least) if we throw a simple or custom error.
-         trace = filter_trace(trace$get(), error_target),
+         ##
+         ## I am not completely sure that we can even rely on stop()
+         ## being in the trace either.
+         trace = trace$get()[-seq_len(stack_skip)],
          warnings = warnings$get())
+  }
+
+  handle_warning <- function(e) {
+    ## Only collect warnings when option
+    if (getOption("warn") == 0) {
+      warnings$add(e$message)
+    }
+    ## But prevent them bubbling up to the top level at the moment
+    tryInvokeRestart("muffleWarning")
   }
 
   tryCatch(
     withCallingHandlers(
       list(success = TRUE,
-           value = with_autoprint(eval(expr, envir)),
+           value = withVisible(eval(expr, envir)),
            warnings = warnings$get()),
-      warning = function(e) warnings$add(e$message),
+      warning = handle_warning,
       error = function(e) trace$add(sys.calls())),
-    error = handler)
+    error = handle_error)
 }
 
 
-filter_trace <- function(trace, target) {
-  if (length(target) == 0) {
-    return(trace)
+## This is generally pretty nasty, and I wonder if it's even
+## worthwhile given how awful warnings are. We could just recommend
+## that users set options(warn = 1) to make them become visible as
+## they appear.
+show_warnings <- function(warnings, success) {
+  n <- length(warnings)
+  nmax <- getOption("nwarnings")
+  if (n == 0) {
+    return()
   }
-  t <- target[[1]]
-  for (i in seq_along(trace)) {
-    x <- trace[[i]]
-    if (is.recursive(x) && identical(x[[1]], t)) {
-      return(filter_trace(trace[-seq_len(i)], target[-1]))
+
+  if (!success) {
+    message("In addition: ", appendLF = FALSE)
+  }
+
+  if (n <= 10) {
+    print.warnings(setNames(vector("list", n), warnings))
+  } else if (n <= nmax) {
+    message(sprintf(ngettext(
+      n,
+      "There was %d warning (use warnings() to see it)",
+      "There were %d warnings (use warnings() to see them)")))
+  } else {
+    message(sprintf(
+      "There were %d or more warnings (use warnings() to see ther first %d)",
+      nmax, nmax))
+  }
+}
+
+
+repl_log_forwarder <- function(path, key) {
+  callr::r_bg(repl_log_forwarder_helper, list(path, key), package = TRUE)
+}
+
+
+repl_log_forwarder_helper <- function(path, key) {
+  con <- redux::hiredis()
+  ## There is probably a nicer way of doing this, but this does seem
+  ## to work. The issue is that we either have to have a blocking
+  ## writer (which is problematic for the host process) or the fifo
+  ## nees to create first. This little mess creates a fifo, then
+  ## starts reading on it.
+  if (!file.exists(path)) {
+    tryCatch(suppressWarnings(fifo(path, "w")), error = function(e) NULL)
+  }
+  p <- fifo(path, "r", blocking = TRUE)
+  while (file.exists(path)) {
+    txt <- readLines(p, 1L)
+    if (length(txt) > 0) {
+      con$RPUSH(key, txt)
     }
   }
-  trace
 }
--- tests/testthat/test-repl.R
+++ tests/testthat/test-repl.R
@@ -8,7 +8,7 @@ test_that("Can evaluate and collect all output", {
     repl_eval(quote(1 + 2), globalenv(), con))
   expect_true(res$success)
   expect_equal(res$warnings, list())
-  expect_equal(res$value, 3)
+  expect_equal(res$value, list(value = 3, visible = TRUE))
   expect_equal(
     readLines(path),
     capture.output(print(3)))
@@ -17,12 +17,14 @@ test_that("Can evaluate and collect all output", {
 
 test_that("Can evaluate and return errors", {
   skip_on_cran()
+  testthat::local_edition(3)
   env <- environment()
 
   f <- function(x) {
     g(x)
   }
   g <- function(x) {
+    message("calculating!")
     h(x)
   }
   h <- function(x) {
@@ -40,35 +42,65 @@ test_that("Can evaluate and return errors", {
     list(con = file(path, "w")),
     repl_eval(f(-1), env, con))
 
-  path <- tempfile()
-  res <- withr::with_connection(
-    list(con = file(path, "w")),
-    repl_eval(f(20), env, con))
-
-  filter_trace(.cmp[[1]], .cmp[[2]])
-
+  expect_equal(res$trace[[1]], quote(f(-1)))
+  expect_equal(res$error,
+               with(list(x = -1), tryCatch(h(x), error = identity)))
+  expect_equal(readLines(path),
+               c("calculating!", "Error in h(x): x must be positive"))
 })
 
 
-test_that("can evaluate and return output/messages", {
-
-})
-
-test_that("Can evaluate and return warnings", {
+test_that("can evaluate and interleave stdout and stderr", {
+  ## This works badly because testthat sets suppressMessages, which
+  ## means that the messages are never emitted. This is fixed in the
+  ## 3rd edition of testthat.
   skip_on_cran()
+  testthat::local_edition(3)
   path <- tempfile()
   res <- withr::with_connection(
     list(con = file(path, "w")),
     repl_eval(quote({
-      message("m1")
-      warning("w1")
-      message("m2")
-      warning("w2")
-      warning("w3")
-      1
+      message("1")
+      cat("2\n")
+      message("3")
+      cat("4\n")
+      cat("5\n")
+      message("6")
     }), globalenv(), con))
+  .GlobalEnv$.res <- res
+  .GlobalEnv$.contents <- readLines(path)
+  expect_equal(readLines(path),
+               as.character(1:6))
+})
 
 
-      1 + 2), globalenv(), con))
+test_that("Can evaluate and return warnings", {
+  skip_on_cran()
+  path <- tempfile()
 
+  f <- function(x) {
+    g(x)
+  }
+  g <- function(x) {
+    for (i in seq_len(abs(x))) {
+      warning("Some warning ", i)
+    }
+    if (x < 0) {
+      stop("x must be positive")
+    }
+    x
+  }
+
+  env <- environment()
+  res <- withr::with_connection(
+    list(con = file(path, "w")),
+    repl_eval(quote(f(3)), env, con))
+  expect_equal(
+    readLines(path),
+    c("[1] 3",
+      "Warning messages:",
+      "1: Some warning 1",
+      "2: Some warning 2",
+      "3: Some warning 3"))
+  expect_equal(res$value, list(value = 3, visible = TRUE))
 })
