e49960ef6d043ded019608532fd2a6a02509cddb
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2021-06-08 11:11:05 +0100

    safe load in python yaml

--- extra/rrq_test.py
+++ extra/rrq_test.py
@@ -5,7 +5,7 @@ import yaml
 r = redis.StrictRedis(host="localhost", port=6379, db=0)
 
 with open("rrq_data.yml", "r") as f:
-    d = yaml.load(f.read())
+    d = yaml.safe_load(f.read())
 
 with open("rrq_data.bin", "rb") as f:
     task_expr = f.read()
