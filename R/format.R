88bb2017a4de1e228d075bded1376fb4f1289107
Rich FitzJohn <rich.fitzjohn@gmail.com>
2023-03-08 09:47:40 +0000

    Start on formatting errors

---
+++ R/format.R
@@ -0,0 +1,22 @@
+##' @export
+format.rrq_task_error <- function(x, width = 180, ...) {
+  call <- conditionCall(x)
+  c("<rrq_task_error>",
+    if (!is.null(call)) {
+      sprintf("   from: %s", deparse1(call, width.cutoff = width))
+    },
+    sprintf("  error: %s", conditionMessage(x)),
+    sprintf("  queue: %s", x$queue_id),
+    sprintf("   task: %s", x$task_id),
+    sprintf(" status: %s", x$status),
+    "  * To throw this error, use stop() with it",
+    if (!is.null(x$trace)) {
+      "  * This error has a stack trace, use '$trace' to see it"
+    })
+}
+
+
+##' @export
+print.rrq_task_error <- function(x, ...) {
+  cat(paste0(format(x, ...), "\n", collapse = ""))
+}
