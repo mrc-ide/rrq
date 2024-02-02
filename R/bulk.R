ef18b3ad5862d0995effc2336be157ed217a7859
Rich FitzJohn <rich.fitzjohn@gmail.com>
2024-02-02 07:38:59 +0000

    Test fixes

--- R/bulk.R
+++ R/bulk.R
@@ -134,7 +134,9 @@ rrq_bulk_wait <- function(con, keys, store, dat, timeout, time_poll, progress,
                     time_poll, progress, dat$key_complete,
                     error = FALSE, follow = follow, single = FALSE)
   if (delete) {
-    task_delete(con, keys, store, dat$task_ids, FALSE)
+    ## TODO: not ideal really
+    controller <- rrq_controller2(keys$queue_id, con)
+    rrq_task_delete(dat$task_ids, check = FALSE, controller)
   }
   if (error) {
     throw_task_errors(ret, single = FALSE)
